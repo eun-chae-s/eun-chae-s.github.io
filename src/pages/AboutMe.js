1e8730101a2750b181bf9e4d5de7c37235df5537
eun-chae-s <s9710173@naver.com>
2023-10-26 23:50:31 -0400

    add bounce icon

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,18 +2,23 @@ import React from "react";
 import smile from "../images/smile.png";
 import Header from "../components/Header";
 import "../css/AboutMe.css";
+import { TiArrowDownThick } from "react-icons/ti";
 
 function AboutMe() {
   return (
     <div>
       <Header></Header>
       <div className="about-me">
+        {/* fix the icon's position */}
         <img className="about-icon" src={smile} alt="Avatar with Smile"></img>
         <div className="about-text">
           A software engineer who wants to <b>contribute</b> to a society for{" "}
           <b>future generations</b>
         </div>
-        <div className="scroll"></div>
+        <div className="scroll">
+          <TiArrowDownThick color="#7cfcda" size={75}></TiArrowDownThick>
+        </div>
+        <div className="stats"></div>
       </div>
     </div>
   );
