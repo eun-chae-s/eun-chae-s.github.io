89e9fc98f545bd6239353da2bd070ec729607f55
eun-chae-s <s9710173@naver.com>
2023-11-11 17:49:24 -0500

    set up the slider and work on card content styling

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "../components/Header";
+import "../css/Projects.css";
 import * as projects from "../data/projects.json";
 import { Card, CardContent, Typography } from "@mui/material";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
 export default function Projects() {
   return (
@@ -9,10 +11,29 @@ export default function Projects() {
       <Header></Header>
       <div className="projects">
         {projects.entities.map((project) => (
-          <Card>
-            <CardContent>
-              <Typography variant="h4" component="div">
+          <Card
+            className="card"
+            sx={{ minWidth: 400, minHeight: 400, borderRadius: 10 }}
+          >
+            <CardContent className="card-content" sx={{ gap: 3 }}>
+              <Typography variant="h6" component="div">
                 {project.title}
+              </Typography>
+              <Typography variant="body3" fontWeight={"bold"}>
+                {project.tech}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {project.description}
+              </Typography>
+              <Typography className="links">
+                <a href={project.github}>
+                  <AiFillGithub size={40}></AiFillGithub>
+                </a>
+                {project.website && (
+                  <a href={project.website}>
+                    <AiOutlineLink size={40}></AiOutlineLink>
+                  </a>
+                )}
               </Typography>
             </CardContent>
           </Card>
