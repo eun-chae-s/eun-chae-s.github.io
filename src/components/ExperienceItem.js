0f0c34592858c16444bac1a78054b3232d0bade2
eun-chae-s <s9710173@naver.com>
2023-12-29 15:58:44 +0900

    finalize the position of timeline and items

--- src/components/ExperienceItem.js
+++ src/components/ExperienceItem.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "../css/ExperienceItem.css";
 
-export default function ExperienceItem() {
+export default function ExperienceItem({ item, direction }) {
   return (
-    <div className="experience-item">
-      <div className="position">test</div>
-      <div className="duration"></div>
-      <div className="description">{/* bullet point */}</div>
+    <div className={"experience-item-" + direction}>
+      <div className="position">
+        {item.position} @ {item.place}
+      </div>
+      <div className="duration">
+        {item.start} ~ {item.end}
+      </div>
+      <ul className="description">
+        {item.description.map((task) => (
+          <li>{task}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,13 +13,27 @@ export default function Experience() {
         <div className="experience-content">
           <div className="experience-left">
             {experience.entities.map((item, index) => {
-              return index % 2 === 0 ? <ExperienceItem /> : <></>;
+              return index % 2 === 0 ? (
+                <div className="left-box">
+                  <ExperienceItem item={item} direction="left" />
+                  <hr className="line"></hr>
+                </div>
+              ) : (
+                <></>
+              );
             })}
           </div>
-          <hr className="timeline"></hr>
+          <div className="timeline"></div>
           <div className="experience-right">
             {experience.entities.map((item, index) => {
-              return index % 2 !== 0 ? <ExperienceItem /> : <></>;
+              return index % 2 !== 0 ? (
+                <div className="right-box">
+                  <hr className="line"></hr>
+                  <ExperienceItem item={item} direction="right" />
+                </div>
+              ) : (
+                <></>
+              );
             })}
           </div>
         </div>
